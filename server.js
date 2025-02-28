db4664fdbf6a8aef8d874239eff9aa48bad128dd
Ankita-Thakur20 <ankitathakur036238@gmail.com>
2025-02-28 14:08:31 +0530

    multer added

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require("express")
 const connectDB = require("./config/db_config")
 const dotenv = require("dotenv").config()
+const multer = require("multer")
+// const upload = multer({dest : "uploads/"})
+
+const storage = multer.diskStorage({destination : (req,file,cb)=>{
+    cb(null,"./uploads")
+},
+filename :(req,file,cb)=>{
+    cb(null,file.originalname)
+}
+})
+
+const upload = multer({storage : storage});
 
 const PORT=process.env.PORT || 5000
 const app = express()
@@ -15,6 +27,12 @@ app.use(express.urlencoded({extended:true}))
 //Route
 app.get("/",(req,res)=>{
     res.send("Welcome")
+})
+
+//multer route
+app.post('/upload',upload.single("files"),(req,res)=>{
+    console.log(req.file)
+    res.send("File uploaded")
 })
 
 //Todo Routes
